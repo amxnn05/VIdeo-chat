47ffdfa788351dd0040902a4371af5cf21e7989f
amxnn05 <amanbora9759345011@gmail.com>
2026-01-28 12:55:43 +0530

    refactor: move google client id to .env

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.tsx'
 
 // REPLACE WITH YOUR ACTUAL GOOGLE CLIENT ID
-const GOOGLE_CLIENT_ID = "187484462364-8t5jb56tqdih6qg8sh3hv8bcbjpugd67.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
